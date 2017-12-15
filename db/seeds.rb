4a87d93c59f583b6d756f71456d6a2bbcf2fb470
caiohvsouza <caiohenriqueads@gmail.com>
2017-12-15 09:38:48 -0300

    Revert "S#3"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
-User.create(nickname: 'André', email: 'andre@speedmsg.com', password: '123123', password_confirmation: '123123')
-User.create(nickname: 'Felipe', email: 'felipe@speedmsg.com', password: '123123', password_confirmation: '123123')
-User.create(nickname: 'Pedro', email: 'Pedro@speedmsg.com', password: '123123', password_confirmation: '123123')
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
